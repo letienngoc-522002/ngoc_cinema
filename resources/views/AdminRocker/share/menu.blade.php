a6e28c1910f5e51b0f32e11906929521589f98f8
ngocle <ngoctrai522002@gmail.com>
2024-08-23 09:03:24 +0700

    initial commit

---
+++ resources/views/AdminRocker/share/menu.blade.php
@@ -0,0 +1,68 @@
+<div class="nav-container primary-menu">
+    <div class="mobile-topbar-header">
+        <div>
+            <img src="https://www.iconeasy.com/icon/png/System/I%20Love/Cinema.png" class="logo-icon" alt="logo icon">
+        </div>
+        <div>
+            <h4 class="logo-text">Stardark</h4>
+        </div>
+        <div class="toggle-icon ms-auto"><i class='bx bx-arrow-to-left'></i>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-xl w-100">
+        <ul class="navbar-nav justify-content-start flex-grow-1 gap-1">
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/cau-hinh/index">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Trang Chủ</b></div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/phim/index-vue">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Phim</b></div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/phong/index">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Phòng</b></div>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/bai-viet/index">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Bài Viết</b></div>
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/lich-chieu/tao-mot-buoi">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Lịch Chiếu</b></div>
+                </a>
+            </li>
+            {{-- <li class="nav-item dropdown">
+                <a href="javascript:;" class="nav-link dropdown-toggle dropdown-toggle-nocaret"
+                    data-bs-toggle="dropdown">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Lịch Chiếu</b></div>
+                </a>
+                <ul class="dropdown-menu">
+                    <li> <a class="dropdown-item" href="/admin/lich-chieu/index"><i
+                                class="bx bx-right-arrow-alt"></i><b>Lặp lại</b></a>
+                    </li>
+                    <li> <a class="dropdown-item" href="/admin/lich-chieu/tao-mot-buoi"><i
+                                class="bx bx-right-arrow-alt"></i><b>Một Buổi</b></a>
+                </ul>
+            </li> --}}
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/khach-hang/thong-tin">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Khách Hàng</b></div>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/admin/lien-he/index">
+                    <div class="menu-title text-uppercase"><b>Quản Lý Liên Hệ</b></div>
+                </a>
+            </li>
+
+
+        </ul>
+    </nav>
+</div>
